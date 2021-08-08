bf5eaa4a732832499c0e95bc64eca113c9b26ef1
Abdurrahman AKKUŞ <a.rahmanakkus@hotmail.com>
2021-08-08 11:09:42 +0300

    Orijinal projenin dosyaları bu projeye entegre edildi, ana index sayfasının controller ve route işlemleri yapıldı

---
+++ resources/views/biz-kimiz.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <link rel="icon" type="image/png" href="images/smaKardesimLogo.svg"/>
+    <title>BİZ KİMİZ</title>
+    <link rel="stylesheet" href="css/nicepage.css" media="screen">
+    <link rel="stylesheet" href="css/biz-kimiz.css" media="screen">
+    <link rel="stylesheet" href="css/custom.css" media="screen">
+    <script class="u-script" type="text/javascript" src="js/jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="js/nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.15.3, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <script type="application/ld+json">
+    {
+        "@context": "http://schema.org",
+        "@type": "Organization",
+        "name": "SMA Kardeşim",
+        "url": "index.php",
+        "logo": "images/default-logo.png"
+    }
+    </script>
+    <meta property="og:title" content="BİZ KİMİZ">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.php">
+    <meta property="og:url" content="index.php">
+</head>
+
+<body class="u-body">
+    <main>
+        <div>
+            <?php include "header.php"; ?>
+            <section class="u-align-left u-clearfix u-section-1" id="sec-de68" style="color: #fff;">
+                <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+                    <div class="fr-view u-clearfix u-rich-text u-text u-text-1">
+                        <h1 style="text-align: left;">
+                            <span style="line-height: 2.0;">Amacımız</span>
+                        </h1>
+                        <p style="text-align: left;">
+                            <span style="line-height: 2.0;">SMA Kardeşim sitesi olarak amacımız tüm SMA'lı çocuklarımızın seslerini yardımseverlere duyurabilmek, çocuklarımızın durumlarına dikkat çekmektir.</span>
+                        </p>
+                        <h4 style="text-align: left;">Ne Yapıyoruz?</h4>
+                        <p style="text-align: left;">
+                            <span style="font-style: italic;">
+                              <ul>
+                                <li>Çocukların hikayelerinin ve kampanya süreçlerinin görülebildiği bir profil oluşturuyoruz.</li>
+                                <li>Gönüllüler ile SMA'lı Çocuklarımızın 'SMA KARDEŞİ' olmalarını sağlıyoruz.</li>
+                                <li>Valilik izin, hastalık raporu gibi resmi belgelerin gönüllülerce görülebilmesini sağlıyoruz.</li>
+                                <li>Gönüllülerin kardeş edindikleri çocukların kampanyalarını sosyal medyalarında paylaşarak daha fazla kişinin dikkatini çekmelerini teşvik ediyoruz.</li>
+                              </ul>
+                            </span>
+                        </p>
+                        <h4 style="text-align: left;">Sorumluluk Reddi</h4>
+                        <p style="text-align: left;">
+                            <span style="font-style: italic;">
+                              <ul>
+                                <li>Bu site üzerinden herhangi bir bağış toplama ve iletme faaliyeti yürütülmemektedir. Site bağışçı ile bağış yapılacak arasındaki bağlantı kurulmasında aracı konumundadır.</li>
+                                <li>Çocuklarımıza ait belgelerin ve bilgilerin içeriklerine ait tüm sorumluluk ve yetki bilgi girişi yapan kişilere aittir.</li>
+                              </ul>
+                            </span>
+                        </p>
+                        <h4 style="text-align: left;">Biz Kimiz</h4>
+                        <p style="text-align: left;">
+                            <span style="font-style: italic;">SMA'lı kardeşlerimiz için bu siteyi geliştiren ve yöneten gönüllüleriz.</span>
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <footer class="u-align-left u-clearfix u-footer" id="sec-0463">
+                <div class="u-clearfix u-sheet u-sheet-1"></div>
+            </footer>
+        </div>
+    </main>
+</body>
+
+</html>
