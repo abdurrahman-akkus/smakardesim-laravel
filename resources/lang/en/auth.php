d7c4e3d1f5e38304fdd6e488a73d9daa5b26477a
Abdurrahman AKKUŞ <a.rahmanakkus@hotmail.com>
2021-08-26 00:18:29 +0300

    Türkçeleştirme dosyaları eklenmiştir

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'Giriş bilgileriniz doğru değil!',
-    'password' => 'Girilen şifre hatalı',
-    'throttle' => 'Aşırı giriş denemesi. Lütfen :seconds saniye sonra yeniden deneyin.',
+    'failed' => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
